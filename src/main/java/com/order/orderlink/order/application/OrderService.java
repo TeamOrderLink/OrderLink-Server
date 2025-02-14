07ccc714f265a4c8b9b9273cb3b31377d0a67fb4
pkl0912 <pkl0912@naver.com>
2025-02-14 15:45:52 +0900

    feat(Order): 주문 생성 기능 구현

--- src/main/java/com/order/orderlink/order/application/OrderService.java
+++ src/main/java/com/order/orderlink/order/application/OrderService.java
@@ -1,9 +1,16 @@
 package com.order.orderlink.order.application;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.order.orderlink.common.client.FoodClient;
 import com.order.orderlink.order.application.dtos.OrderRequest;
+import com.order.orderlink.order.domain.Order;
+import com.order.orderlink.order.domain.OrderStatus;
+import com.order.orderlink.order.domain.repository.OrderRepository;
+import com.order.orderlink.orderitem.domain.OrderItem;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +19,32 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
 	private final FoodClient foodClient;
+	private final OrderRepository orderRepository;
 
-	public String createOrder(OrderRequest.Create request) {
-		System.out.println("request = " + request.getValue());
-		System.out.println("오더 생성을 위해 상품을 땡겨와 봅시다 !! :) ~ " + foodClient.getFoods());
-		return "createOrder() has been called!";
+	@Transactional
+	public void createOrder(OrderRequest.Create request) {
+		// 주문 객체 생성
+		Order order = Order.builder()
+			.id(UUID.randomUUID())
+			.restaurantId(request.getRestaurantId())
+			.orderType(request.getOrderType())
+			.totalPrice(request.getTotalPrice())
+			.deliveryAddress(request.getDeliveryAddress())
+			.deliveryRequest(request.getDeliveryRequest())
+			.status(OrderStatus.NEW)
+			.build();
+
+		request.getFoods().forEach(food -> {
+			OrderItem orderItem = OrderItem.builder()
+				.id(UUID.randomUUID())
+				.order(order)
+				.foodName(food.getFoodName())
+				.quantity(food.getFoodCount())
+				.price(food.getFoodPrice())
+				.build();
+			order.addOrderItem(orderItem);
+		});
+
+		orderRepository.save(order);
 	}
-
 }
