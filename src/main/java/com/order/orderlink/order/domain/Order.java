f93b9c9a5470e933b3de3e1fab7046502512b10d
pkl0912 <pkl0912@naver.com>
2025-02-14 15:44:03 +0900

    refactor(Order): 연관관계 매핑

--- src/main/java/com/order/orderlink/order/domain/Order.java
+++ src/main/java/com/order/orderlink/order/domain/Order.java
@@ -1,6 +1,7 @@
 package com.order.orderlink.order.domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.UuidGenerator;
@@ -10,10 +11,18 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.order.orderlink.orderitem.domain.OrderItem;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +35,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "orders")
 public class Order {
 
 	@Id
@@ -33,18 +43,26 @@ public class Order {
 	private UUID id;
 
 	@NotNull
-	private UUID userId;
-	@NotNull
 	private UUID restaurantId;
+
 	@NotNull
 	private String deliveryAddress;
+
 	private String deliveryRequest;
+
 	@NotNull
 	private Integer totalPrice;
+
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
+
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	private OrderType orderType;
+
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	private List<OrderItem> orderItems;
 
 	@CreatedDate
 	@Column(updatable = false)
@@ -62,4 +80,9 @@ public class Order {
 
 	private LocalDateTime deletedAt;
 	private UUID deletedBy;
+
+	public void addOrderItem(OrderItem orderItem) {
+		this.orderItems.add(orderItem);
+	}
 }
+
