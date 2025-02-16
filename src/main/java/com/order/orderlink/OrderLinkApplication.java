d9818749b678d10cd9f2a991fba989f37ba4b378
pkl0912 <pkl0912@naver.com>
2025-02-16 14:56:04 +0900

    refactor(Common): EnableJpaAuditing 충돌 제거

--- src/main/java/com/order/orderlink/OrderLinkApplication.java
+++ src/main/java/com/order/orderlink/OrderLinkApplication.java
@@ -2,10 +2,8 @@ package com.order.orderlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class OrderLinkApplication {
