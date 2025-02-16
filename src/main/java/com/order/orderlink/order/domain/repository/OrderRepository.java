75ddf0a1fb6680adae06911c09708462f694b873
pkl0912 <pkl0912@naver.com>
2025-02-16 21:50:24 +0900

    refactor(Order): uuid 로 수정

--- src/main/java/com/order/orderlink/order/domain/repository/OrderRepository.java
+++ src/main/java/com/order/orderlink/order/domain/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.order.orderlink.order.domain.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.order.orderlink.order.domain.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 }
