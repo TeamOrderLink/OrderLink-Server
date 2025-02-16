9d9f98bfeaa7ab169fc1ba7aa84cd87653dd23b7
pkl0912 <pkl0912@naver.com>
2025-02-16 14:51:52 +0900

    chore(Common): WebClient 메소드 나중에 쓸 것 추가

--- src/main/java/com/order/orderlink/common/client/UserClient.java
+++ src/main/java/com/order/orderlink/common/client/UserClient.java
@@ -1,7 +1,10 @@
 package com.order.orderlink.common.client;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +13,21 @@ import lombok.RequiredArgsConstructor;
 public class UserClient {
 
 	private final WebClient webClient;
+
+	private static final String USERS_URL = "/users";
+
+	public boolean isRestaurantExists(UUID userId) {
+		try {
+			webClient.get()
+				.uri(uriBuilder -> uriBuilder.path(USERS_URL + "/{userId}").build(userId))
+				.retrieve()
+				.toBodilessEntity()
+				.block();
+
+			return true;
+		} catch (WebClientResponseException.NotFound e) {
+			return false;
+		}
+	}
 
 }
