21d46a16506f7cbfe4b3e161dd80d841459d9f93
pkl0912 <pkl0912@naver.com>
2025-02-16 14:53:53 +0900

    chore(Order): 도메인 추가

--- src/main/java/com/order/orderlink/order/domain/Order.java
+++ src/main/java/com/order/orderlink/order/domain/Order.java
@@ -1,25 +1,20 @@
 package com.order.orderlink.order.domain;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.UuidGenerator;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.order.orderlink.common.entity.BaseTimeEntity;
 import com.order.orderlink.orderitem.domain.OrderItem;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -34,13 +29,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "p_orders")
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "orders")
-public class Order {
+public class Order extends BaseTimeEntity {
 
 	@Id
-	@UuidGenerator
+	@UuidGenerator(style = UuidGenerator.Style.AUTO)
 	private UUID id;
+
+	@NotNull
+	private UUID userId;
 
 	@NotNull
 	private UUID restaurantId;
@@ -61,28 +59,12 @@ public class Order {
 	@Enumerated(EnumType.STRING)
 	private OrderType orderType;
 
-	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-	private List<OrderItem> orderItems;
-
-	@CreatedDate
-	@Column(updatable = false)
-	private LocalDateTime createdAt;
-
-	@LastModifiedDate
-	private LocalDateTime updatedAt;
-
-	@CreatedBy
-	@Column(updatable = false)
-	private UUID createdBy;
-
-	@LastModifiedBy
-	private UUID updatedBy;
-
-	private LocalDateTime deletedAt;
-	private UUID deletedBy;
+	@Builder.Default
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+	private List<OrderItem> orderItems = new ArrayList<>();
 
 	public void addOrderItem(OrderItem orderItem) {
-		this.orderItems.add(orderItem);
+		orderItem.setOrder(this);
+		orderItems.add(orderItem);
 	}
 }
-
--- src/main/java/com/order/orderlink/order/domain/OrderStatus.java
+++ src/main/java/com/order/orderlink/order/domain/OrderStatus.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum OrderStatus {
-    NEW("주문 등록"), APPROVED("주문 승인"), CANCLED("주문 취소"), DELIVERY("배달 중");
+	NEW("주문 등록"), APPROVED("주문 승인"), CANCELED("주문 취소"), DELIVERY("배달 중");
 
-    private final String value;
+	private final String value;
 }
