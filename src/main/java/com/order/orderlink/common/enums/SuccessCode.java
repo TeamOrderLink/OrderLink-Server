c700443143f4a071d726c84821d9569d9a523096
JihwanB <leyqorwlghks@gmail.com>
2025-02-17 12:03:30 +0900

    feat(User): 회원가입 기능 추가, 요청과 응답을 일관성 있게 리팩터링, 불필요한 파일 제거

--- src/main/java/com/order/orderlink/common/enums/SuccessCode.java
+++ src/main/java/com/order/orderlink/common/enums/SuccessCode.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
+
+	USER_CREATE_SUCCESS(HttpStatus.OK, "회원가입이 완료되었습니다."),
 	ORDER_CREATE_SUCCESS(HttpStatus.OK, "주문 생성 성공입니다."),
 	ORDER_GET_SUCCESS(HttpStatus.OK, "주문 목록 조회 성공입니다.");
 
---
+++ src/main/java/com/order/orderlink/user/application/dtos/UserResponse.java
@@ -0,0 +1,32 @@
+package com.order.orderlink.user.application.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponse {
+
+	@Getter
+	@AllArgsConstructor
+	public static class Create {
+		private UUID id;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	public static class Read {
+		private UUID id;
+		private String username;
+		private String email;
+		private String phone;
+		private String nickname;
+		private String role;
+		private Boolean isPublic;
+		private LocalDateTime createdAt;
+	}
+
+}
