7f828ef4b7cab2bbe597fbcfcb5b88ecbde29f1c
pkl0912 <pkl0912@naver.com>
2025-02-17 12:52:50 +0900

    chore(Restaurant): domain 수정

--- src/main/java/com/order/orderlink/restaurant/domain/Restaurant.java
+++ src/main/java/com/order/orderlink/restaurant/domain/Restaurant.java
@@ -1,12 +1,32 @@
 package com.order.orderlink.restaurant.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import java.util.UUID;
 
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "p_restaurants")
+@EntityListeners(AuditingEntityListener.class)
 public class Restaurant {
 
 	@Id
-	private Long id;
-	
+	@UuidGenerator(style = UuidGenerator.Style.AUTO)
+	private UUID id;
+
+	private String name;
+
 }
--- src/main/java/com/order/orderlink/restaurant/domain/repository/RestaurantRepository.java
+++ src/main/java/com/order/orderlink/restaurant/domain/repository/RestaurantRepository.java
@@ -1,9 +1,11 @@
 package com.order.orderlink.restaurant.domain.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.order.orderlink.restaurant.domain.Restaurant;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-	
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
+
 }
