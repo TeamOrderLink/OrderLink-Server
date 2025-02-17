611953e363a453d1967dcbbc2a9d71ceaf7778aa
pkl0912 <pkl0912@naver.com>
2025-02-17 12:50:40 +0900

    Chore(Common): enums 추가

--- src/main/java/com/order/orderlink/common/enums/SuccessCode.java
+++ src/main/java/com/order/orderlink/common/enums/SuccessCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-	ORDER_CREATE_SUCCESS(HttpStatus.OK, "주문 생성 성공입니다.");
+	ORDER_CREATE_SUCCESS(HttpStatus.OK, "주문 생성 성공입니다."),
+	ORDER_GET_SUCCESS(HttpStatus.OK, "주문 목록 조회 성공입니다.");
 
 	private final HttpStatus status;
 	private final String message;
