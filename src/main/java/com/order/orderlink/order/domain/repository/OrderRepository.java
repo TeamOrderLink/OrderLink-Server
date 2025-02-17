1ea9c5d8f74e21f8ab2f92fece7db767d503b301
pkl0912 <pkl0912@naver.com>
2025-02-17 12:52:03 +0900

    chore(Order): repository 주문 찾기 메소드 추가

--- src/main/java/com/order/orderlink/order/domain/repository/OrderRepository.java
+++ src/main/java/com/order/orderlink/order/domain/repository/OrderRepository.java
@@ -2,9 +2,13 @@ package com.order.orderlink.order.domain.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.order.orderlink.order.domain.Order;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+	Page<Order> findAllByUserId(UUID userId, Pageable pageable);
 }
