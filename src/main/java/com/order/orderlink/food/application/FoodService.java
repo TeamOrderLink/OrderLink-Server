e67eecf96502c6b0d673aa6ea007c9d6ae4f58e5
hyeonhee <nloavee@gmail.com>
2025-02-18 16:31:11 +0900

    feat(Food): Implement createFood service

--- src/main/java/com/order/orderlink/food/application/FoodService.java
+++ src/main/java/com/order/orderlink/food/application/FoodService.java
@@ -1,8 +1,41 @@
 package com.order.orderlink.food.application;
 
+import com.order.orderlink.common.enums.ErrorCode;
+import com.order.orderlink.common.exception.RestaurantException;
+import com.order.orderlink.food.application.dtos.FoodRequest;
+import com.order.orderlink.food.application.dtos.FoodResponse;
+import com.order.orderlink.food.domain.Food;
+import com.order.orderlink.food.domain.repository.FoodRepository;
+import com.order.orderlink.restaurant.domain.Restaurant;
+import com.order.orderlink.restaurant.domain.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class FoodService {
-	
+
+    private final FoodRepository foodRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    public FoodResponse.Create createFood(UUID restaurantId, FoodRequest.Create request) {
+
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new RestaurantException(ErrorCode.RESTAURANT_NOT_FOUND));
+        Food food = Food.builder()
+                .restaurant(restaurant)
+                .name(request.getName())
+                .description(request.getDescription())
+                .price(request.getPrice())
+                .imageUrl(request.getImageUrl())
+                .isHidden(request.isHidden())
+                .build();
+
+        Food savedFood = foodRepository.save(food);
+
+        return new FoodResponse.Create(savedFood.getId());
+    }
+
 }
