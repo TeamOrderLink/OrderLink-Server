66d5e59cc7fddf3bd60739282c936c979e9abecf
9yuhyeon <kkh2742@gmail.com>
2025-02-19 21:39:09 +0900

    chore(Restaurant): RequestDto 정규식 검증 추가, 카테고리 및 리전 UUID -> String 타입 변경, String 타입의 businessHours -> LocalTime 타입으로 openTime, closeTime 구분하여 변경

--- src/main/java/com/order/orderlink/restaurant/application/dtos/RestaurantRequest.java
+++ src/main/java/com/order/orderlink/restaurant/application/dtos/RestaurantRequest.java
@@ -1,8 +1,11 @@
 package com.order.orderlink.restaurant.application.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,25 +19,36 @@ public class RestaurantRequest {
         @NotBlank
         private String address;
 
+        @Pattern(regexp = "^[0-9]{10,11}$", message = "-를 제외한 10자리 또는 11자리 숫자로 입력해주세요.")
         @NotBlank
         private String phone;
 
         private String description;
 
         @NotBlank
-        private String businessHours;
+        @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "영업 시간은 00:00 ~ 23:59까지만 입력 가능합니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        private LocalTime openTime;
+
+        @NotBlank
+        @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "영업 시간은 00:00 ~ 23:59까지만 입력 가능합니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        private LocalTime closeTime;
 
         @NotBlank
         private String ownerName;
 
         @NotBlank
+        @Pattern(regexp = "^\\d{3}-\\d{2}-\\d{5}$", message = "사업자 등록 번호는 '-'를 포함하여 000-00-00000 형식으로 입력해야 합니다.")
         private String businessRegNum;
 
         @NotBlank
-        private UUID categoriesId;
+        @Pattern(regexp = "^[가-힣]+$", message = "카테고리는 한글만 입력 가능합니다.")
+        private String categories;
 
         @NotBlank
-        private UUID regionId;
+        @Pattern(regexp = "^[가-힣]+$", message = "지역명은 한글만 입력 가능합니다.")
+        private String region;
 
         @NotBlank
         private List<RestaurantFoodDto> foods;
