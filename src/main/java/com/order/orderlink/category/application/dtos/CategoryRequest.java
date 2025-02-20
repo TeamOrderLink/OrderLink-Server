dd5dd00720e05a954d10b44668735fc6d587e390
pkl0912 <pkl0912@naver.com>
2025-02-20 12:21:56 +0900

    chore(Category): dto 추가

---
+++ src/main/java/com/order/orderlink/category/application/dtos/CategoryRequest.java
@@ -0,0 +1,23 @@
+package com.order.orderlink.category.application.dtos;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+public class CategoryRequest {
+
+	@Getter
+	public static class Create {
+
+		@NotBlank
+		private String name;
+	}
+
+	@Getter
+	public static class RegisterRestaurantCategory {
+
+		private List<UUID> categoryIds;
+	}
+}
