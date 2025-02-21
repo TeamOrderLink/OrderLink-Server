67080083de6d63081b3cd1688f9a44cc95f7e382
JihwanB <leyqorwlghks@gmail.com>
2025-02-21 12:51:47 +0900

    feat(AI): AI를 통해 음식 설명을 추천받는 API 추가

--- src/main/java/com/order/orderlink/common/enums/ErrorCode.java
+++ src/main/java/com/order/orderlink/common/enums/ErrorCode.java
@@ -34,8 +34,11 @@ public enum ErrorCode {
 	//Category
 	CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리가 존재하지 않습니다."),
 
-    // FOOD
-    FOOD_NOT_FOUND(HttpStatus.NOT_FOUND, "음식을 찾을 수 없습니다.");
+	// FOOD
+	FOOD_NOT_FOUND(HttpStatus.NOT_FOUND, "음식을 찾을 수 없습니다."),
+
+	// AI
+	AI_API_RESPONSE_PARSING_ERROR(HttpStatus.NOT_FOUND, "음식 설명 생성 도중 오류가 발생했습니다.");
 
 	private final HttpStatus status;
 	private final String message;
--- src/main/java/com/order/orderlink/common/enums/SuccessCode.java
+++ src/main/java/com/order/orderlink/common/enums/SuccessCode.java
@@ -34,7 +34,7 @@ public enum SuccessCode {
 
 	// FOOD
 	FOOD_CREATE_SUCCESS(HttpStatus.OK, "음식 등록 성공입니다."),
-    FOOD_UPDATE_SUCCESS(HttpStatus.OK, "음식 수정 성공입니다."),
+	FOOD_UPDATE_SUCCESS(HttpStatus.OK, "음식 수정 성공입니다."),
 
 	// ADDRESS
 	ADDRESS_CREATE_SUCCESS(HttpStatus.OK, "배송지 등록 성공입니다."),
@@ -43,12 +43,15 @@ public enum SuccessCode {
 	ADDRESS_UPDATE_SUCCESS(HttpStatus.OK, "배송지 수정 성공입니다."),
 	ADDRESS_DELETE_SUCCESS(HttpStatus.OK, "배송지 삭제 성공입니다."),
 
-	//Category
+	// CATEGORY
 	CATEGORY_CREATE_SUCCESS(HttpStatus.OK, "카테고리 새로 추가 성공입니다"),
 	CATEGORY_REGISTER_SUCCESS(HttpStatus.OK, "음식점 카테고리 등록 성공입니다"),
 	CATEGORY_GET_SUCCESS(HttpStatus.OK, "카테고리 리스트 조회 성공입니다"),
 	CATEGORY_DELETE_SUCCESS(HttpStatus.OK, "카테고리 삭제 성공입니다"),
-	CATEGORY_UPDATE_SUCCESS(HttpStatus.OK, "카테고리 수정 성공입니다");
+	CATEGORY_UPDATE_SUCCESS(HttpStatus.OK, "카테고리 수정 성공입니다"),
+
+	// AI
+	AI_DESCRIPTION_GENERATE_SUCCESS(HttpStatus.OK, "음식 설명 생성 성공입니다.");
 
 	private final HttpStatus status;
 	private final String message;
