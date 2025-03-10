1077719d111f272ee10251b5357a166dcc80365c
JihwanB <leyqorwlghks@gmail.com>
2025-03-10 10:37:48 +0900

    refactor(Address): Request와 Response에 private constructor 추가

--- src/main/java/com/order/orderlink/address/application/dtos/AddressResponse.java
+++ src/main/java/com/order/orderlink/address/application/dtos/AddressResponse.java
@@ -4,7 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -12,13 +14,15 @@ import lombok.Getter;
 public class AddressResponse {
 
 	@Getter
-	@AllArgsConstructor
+	@Builder
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	public static class Create {
 		private UUID id;
 	}
 
 	@Getter
-	@AllArgsConstructor
+	@Builder
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	public static class ReadAll {
 		private List<AddressResponse.Read> addresses;
 		private int currentPage;
@@ -27,7 +31,8 @@ public class AddressResponse {
 	}
 
 	@Getter
-	@AllArgsConstructor
+	@Builder
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	public static class Read {
 		private UUID id;
 		private String address;
@@ -36,7 +41,8 @@ public class AddressResponse {
 	}
 
 	@Getter
-	@AllArgsConstructor
+	@Builder
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	public static class Update {
 		private UUID id;
 		private String address;
